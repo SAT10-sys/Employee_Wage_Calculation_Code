4979e0401eb0351d882fc6b0d2f50f837131e409
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 11:02:18 +0530

    [satantra] wage computed using class methods

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ namespace Employee_Wage_Calculation_Code
 {
     class Program
     {
-        // constants
+
         public const int IS_FULL_TIME = 2;
         public const int IS_PART_TIME = 1;
         public const int EMP_RATE_PER_HR = 20;
         public const int NUM_WRKNG_DAYS = 2;
         public const int MAX_HRS = 10;
-        static void Main(string[] args)
+        public static int computeWage()
         {
             //variables
             int empHr = 0;
@@ -37,7 +37,13 @@ namespace Employee_Wage_Calculation_Code
                 Console.WriteLine(" Days: " + totalWrkDay + " Employee Hours: " + empHr);
             }
             int totEmpWage = totalEmpHr * EMP_RATE_PER_HR;
-            Console.WriteLine(" Total Employee Wage: " + totEmpWage);
+            Console.WriteLine(" Total Employee Wage is: " + totEmpWage);
+            return totEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeWage();
         }
     }
+
 }
