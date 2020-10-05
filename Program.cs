c212e24ed153009da33162a8e4a8559940a15f7e
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 10:32:16 +0530

    [satantra] checked employee is present or absent

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace Employee_Wage_Calculation_Code
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Wage Calculation Program ");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empChk = random.Next(0, 2);
+            if (empChk == IS_FULL_TIME)
+            {
+                Console.WriteLine(" Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine(" Employee is Absent ");
+            }
         }
     }
 }
