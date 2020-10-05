a044577461310ed6ab27f7cc082dde9f31da8be8
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 10:29:36 +0530

    [satantra] welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Calculation_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Employee Wage Calculation Program ");
+        }
+    }
+}
