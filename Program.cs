b8edba59fcefa3c0b76cf2901e407a9c22ab3cdf
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 10:38:42 +0530

    [satantra] part time employee added and wages calculated

--- Program.cs
+++ Program.cs
@@ -4,16 +4,21 @@ namespace Employee_Wage_Calculation_Code
 {
     class Program
     {
-        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HR = 20;
         static void Main(string[] args)
         {
             // variables
-            int empHr;
-            int empWage;
+            int empHr = 0;
+            int EmpWage = 0;
             Random random = new Random();
-            int empChk = random.Next(0, 2);
-            if (empChk == IS_FULL_TIME)
+            int empChk = random.Next(0, 3);
+            if (empChk == IS_PART_TIME)
+            {
+                empHr = 4;
+            }
+            else if (empChk == IS_FULL_TIME)
             {
                 empHr = 8;
             }
@@ -21,8 +26,8 @@ namespace Employee_Wage_Calculation_Code
             {
                 empHr = 0;
             }
-            empWage = empHr * EMP_RATE_PER_HR;
-            Console.WriteLine(" Employee Wage is: " + empWage);
+            EmpWage = empHr * EMP_RATE_PER_HR;
+            Console.WriteLine(" Employee Wage is: " + EmpWage);
         }
     }
 }
