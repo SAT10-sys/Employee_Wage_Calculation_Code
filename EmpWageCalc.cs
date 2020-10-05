76cbfc744933653fc65d65d3d745cc31459f5595
Satantra Tewari <satantra1508@gmail.com>
2020-10-05 11:12:20 +0530

    [satantra] total wages saved using class

---
+++ EmpWageCalc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Calculation_Code
+{
+    class EmpWageCalc
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+        public EmpWageCalc(string company, int empRateperHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRateperHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void computeWage()
+        {
+            int empHr = 0, totEmpHrs = 0, totWorkingDays = 0;
+            while (totEmpHrs <= this.maxHoursPerMonth && totWorkingDays < this.numOfWorkingDays)
+            {
+                totWorkingDays++;
+                Random random = new Random(0);
+                int empChk = random.Next(0, 2);
+                switch (empChk)
+                {
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                totEmpHrs += empHr;
+                Console.WriteLine(" Days: " + totWorkingDays + " Employee Hours: " + empHr);
+            }
+            int totEmpWage = totEmpHrs * this.empRatePerHour;
+            Console.WriteLine(" Total Employee Wage for company: " + company + " is: " + totEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Employee Wage For Company: " + company + " is: " + totalEmpWage;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,41 +4,16 @@ namespace Employee_Wage_Calculation_Code
 {
     class Program
     {
-        // constants
-        public const int IS_FULL_TIME = 2;
-        public const int IS_PART_TIME = 1;
-        public static int computeWage(string company, int empRatePerHour, int numWorkingDay, int maxHrsPerMonth)
-        {
-            int empHr = 0, totEmpHrs = 0, totWorkingDays = 0;
-            while (totEmpHrs <= maxHrsPerMonth && totWorkingDays < numWorkingDay)
-            {
-                totWorkingDays++;
-                Random random = new Random(0);
-                int empChk = random.Next(0, 2);
-                switch (empChk)
-                {
-                    case IS_PART_TIME:
-                        empHr = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHr = 8;
-                        break;
-                    default:
-                        empHr = 0;
-                        break;
-                }
-                totEmpHrs += empHr;
-                Console.WriteLine(" Days: " + totWorkingDays + " Employee Hours: " + empHr);
-            }
-            int totEmpWage = totEmpHrs * empRatePerHour;
-            Console.WriteLine(" Total Employee Wage for company: " + company + " is: " + totEmpWage);
-            return totEmpWage;
-        }
         static void Main(string[] args)
         {
-            computeWage("Mart", 20, 2, 10);
-            computeWage("Reliance", 10, 4, 20);
+            EmpWageCalc mart = new EmpWageCalc("Mart", 10, 2, 20);
+            EmpWageCalc reliance = new EmpWageCalc("Reliance", 10, 4, 20);
+            mart.computeWage();
+            Console.WriteLine(mart.toString());
+            reliance.computeWage();
+            Console.WriteLine(reliance.toString());
         }
+            
     }
 
 }
